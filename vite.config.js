5c88042ac300769c32da6962387af9de5a33e0eb
Matías Surdi <matiassurdi@gmail.com>
2021-07-31 17:36:08 +0200

    Fix xstate inspector not loading

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+// import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+
+const crossOriginIsolation = () => ({
+  name: "configure-server",
+
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  },
+});
 
 export default defineConfig({
   plugins: [vue(), crossOriginIsolation()],

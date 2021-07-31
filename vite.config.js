72b9cd10b88df8d6f81660f062325113a85bc446
Matías Surdi <matiassurdi@gmail.com>
2021-07-31 18:02:43 +0200

    Fix headers preventing wasm module to load

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const crossOriginIsolation = () => ({
 
   configureServer(server) {
     server.middlewares.use((_req, res, next) => {
-      res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
       res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
       next();
     });

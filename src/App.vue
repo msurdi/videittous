502bed704e2cd18f0e4e4a76395a0278700976b3
Matías Surdi <matiassurdi@gmail.com>
2021-08-01 11:58:06 +0200

    Add Github link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="bg-black text-white p-4 font-bold text-xl">
+  <header
+    class="bg-black text-white p-4 font-bold text-xl flex justify-between"
+  >
     <router-link :to="{ name: 'landing' }">Videittous</router-link>
+    <a href="https://github.com/msurdi/videittous">
+      <vue-feather class="px-2" type="github" />
+    </a>
   </header>
 
   <router-view></router-view>

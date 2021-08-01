333601ad18905ed86685ab733a5ab6a2ea35eac9
Matías Surdi <matiassurdi@gmail.com>
2021-08-01 10:25:47 +0200

    Fix feature list items alignment

--- src/components/FeatureListItem.vue
+++ src/components/FeatureListItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <li class="flex">
-    <vue-feather class="px-2 py-1" type="check" stroke="green" />
+  <li class="flex py-1">
+    <vue-feather class="px-2" type="check" stroke="green" />
     <slot />
   </li>
 </template>

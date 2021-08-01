0910347af91d356c207e83e6844a146aebe2b6ce
Matías Surdi <matiassurdi@gmail.com>
2021-08-01 10:24:38 +0200

    Implement custom server to add necessary headers for wasm module loading

---
+++ server/index.js
@@ -0,0 +1,37 @@
+require("express-async-errors");
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT ?? 8080;
+const ADDRESS = process.env.ADDRESS ?? "0.0.0.0";
+
+const STATIC_DIR = path.resolve(__dirname, "../dist");
+const INDEX_FILE = path.resolve(STATIC_DIR, "index.html");
+const devMode = process.env.NODE_ENV === "development";
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(express.static(STATIC_DIR));
+app.use(
+  helmet({
+    contentSecurityPolicy: !devMode,
+    hsts: !devMode,
+  })
+);
+
+app.get("*", (req, res) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.sendFile(INDEX_FILE);
+});
+
+app.listen(PORT, ADDRESS, () => {
+  // eslint-disable-next-line no-console
+  console.log("Server up and running on ", `http://${ADDRESS}:${PORT}/`);
+});

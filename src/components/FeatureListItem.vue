02b2f4495b9a7fcb7426644afc1c8e2e70dd1c28
Matías Surdi <matiassurdi@gmail.com>
2021-08-02 08:52:53 +0200

    Improve landing page

--- src/components/FeatureListItem.vue
+++ src/components/FeatureListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="flex py-1">
-    <vue-feather class="px-2" type="check" stroke="green" />
+    <vue-feather class="px-2" type="check-circle" stroke="green" />
     <slot />
   </li>
 </template>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,32 +1,33 @@
 <template>
-  <main class="flex flex-col items-center mt-12 px-4 max-w-xl mx-auto">
+  <main class="flex flex-col items-center my-5 px-4 max-w-xl mx-auto">
     <div class="text-6xl font-bold">Videittous</div>
-    <div class="">The easiest way to optimize any video file</div>
-    <div class="text-base mt-12">
-      <p class="my-6">
-        Videittous allows you to optimize any video you might have so that it
-        can be played anywhere at the minimum size possible.
-      </p>
-      <FeatureList>
-        <FeatureListItem>Easy to use.</FeatureListItem>
-        <FeatureListItem
-          >No ads, no watermarks, we don't save your videos.</FeatureListItem
-        >
-        <FeatureListItem>It's free. </FeatureListItem>
-        <FeatureListItem
-          >The video is never uploaded, all the work is performed in your
-          browser.
-        </FeatureListItem>
-        <FeatureListItem
-          >Only limited by your own computer's power.
-        </FeatureListItem>
-      </FeatureList>
+    <div class="text-base my-5">
+      <p class="my-5">Convert any video in a small, widely compatible MP4.</p>
     </div>
-    <div class="mt-12">
+    <div class="my-5">
       <router-link :to="{ name: 'encode' }" class="btn btn-success"
         >Get started</router-link
       >
     </div>
+    <FeatureList class="my-5">
+      <FeatureListItem>Easy to use.</FeatureListItem>
+      <FeatureListItem
+        >No ads, no watermarks, we don't save your videos.</FeatureListItem
+      >
+      <FeatureListItem>It's free. </FeatureListItem>
+      <FeatureListItem
+        >The video is never uploaded, all the work is performed in your browser.
+      </FeatureListItem>
+      <FeatureListItem
+        >Only limited by your own computer's power.
+      </FeatureListItem>
+      <FeatureListItem>No registration and no login required.</FeatureListItem>
+      <FeatureListItem
+        ><a class="link" href="https://github.com/msurdi/videittous"
+          >Open source</a
+        >.</FeatureListItem
+      >
+    </FeatureList>
   </main>
 </template>
 
